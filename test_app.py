64989a3dee1a62f58eba417a002a9111f024592d
“ASHLEY” <ashlyyy6969@gmail.com>
2025-05-19 03:42:31 +0530

    tests

--- test_app.py
+++ test_app.py
@@ -2,7 +2,6 @@ from app import app
 
 def test_home():
     response=app.test_client().get("/")
-    
+
     assert response.status_code==200
-    assert response.data==b"Hello World!"
-    
+    assert response.data== b"Hello World!"
